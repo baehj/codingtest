d554cdb585d28109ed8dedba8e9ffa8118d19315
baehj <101856005+baehj@users.noreply.github.com>
2022-07-12 11:36:28 +0900

    220712 전화번호 목록 / for문, startsWith

--- programmers/src/java_level2/test3_1.java
+++ programmers/src/java_level2/test3_1.java
@@ -9,7 +9,7 @@ public class test3_1 {
 		String [] phone_book = {"12","123","1235","567","88"};
 		boolean answer = true;
 		
-		// 풀이 1) for문 
+		// 풀이 1) for문, startsWith
 		Arrays.sort(phone_book); // -> sorting을 굳이 한 이유 : 접두어 포함된 번호가 붙어나와 빨리 찾을 수 있을 것 같아서.
 		
 		for (int i = 0; i < phone_book.length - 1; i++) {
