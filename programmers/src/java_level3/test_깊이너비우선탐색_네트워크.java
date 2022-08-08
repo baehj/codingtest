3438873984c87049e0a4be11c11ef9214d2da659
baehj <101856005+baehj@users.noreply.github.com>
2022-08-08 22:20:30 +0900

    220808 lv3 깊이너비우선탐색_네트워크

---
+++ programmers/src/java_level3/test_깊이너비우선탐색_네트워크.java
@@ -0,0 +1,27 @@
+package java_level3;
+
+public class test_깊이너비우선탐색_네트워크 {
+	class Solution {
+	    public int solution(int n, int[][] computers) {
+	        int answer = 0;
+	        boolean[] check = new boolean[n];
+	        for (int i=0; i<n; i++){
+	            if(!check[i]){
+	                dfs(computers, i, check);
+	                answer++;
+	            }
+	        }
+	        return answer;
+	    }
+	    
+	    boolean[] dfs(int[][] computers, int i, boolean[] check) {
+	        check[i]= true;
+	            for(int j=0; j<computers.length; j++){
+	               if (i != j && computers[i][j] == 1 && check[j] == false) {
+	                   check = dfs(computers, j, check);
+	                }
+	            }
+	        return check;
+	    }
+	}
+}
