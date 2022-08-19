d032a79b24faa6dc999cdba3c979024cfa41ba5e
baehj <101856005+baehj@users.noreply.github.com>
2022-08-19 23:53:18 +0900

    220819 백준 11050번 문제, 유형 : 위상정렬(=그래프 정렬)

--- baekjoon/src/javajava/test_11050.java
+++ baekjoon/src/javajava/test_11050.java
@@ -1,7 +1,7 @@
 package javajava;
 
 public class test_11050 {
-	// 백준 11050번 문제, 유형 : 위상정렬(=그래프 정렬)
+	// 백준 11050번 문제, 유형 : 위상정렬(=그래프 정렬) 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
