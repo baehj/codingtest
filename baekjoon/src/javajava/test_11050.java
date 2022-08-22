af9b12af0f834707310f5a327f868ba66fda09af
baehj <101856005+baehj@users.noreply.github.com>
2022-08-22 22:36:10 +0900

    220822 백준 11050번 문제, 유형 : 조합 기본 문제(점화식 이용)

--- baekjoon/src/javajava/test_11050.java
+++ baekjoon/src/javajava/test_11050.java
@@ -1,10 +1,33 @@
 package javajava;
 
-public class test_11050 {
-	// 백준 11050번 문제, 유형 : 위상정렬(=그래프 정렬) 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.util.Scanner;
 
+public class test_11050 {
+	// 백준 11050번 문제, 유형 : 조합 기본 문제(점화식 이용)
+	
+	static int N, K;
+	static int D[][];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		K = sc.nextInt();
+		D = new int[N+1][N+1];
+		// 초기화
+		for(int i=0; i<=N; i++) {
+			D[i][i] = 1;
+			D[i][0] = 1;
+			D[i][1] = i;
+		}
+		
+		// 점화식으로 배열 완성하기 - bottom up 방식으로
+		for(int i=2; i<=N; i++) { // 0,1은 초기화 된 상태.
+			for(int j=1; j<i; j++) {
+				D[i][j] = D[i-1][j] + D[i-1][j-1]; // 조합 점화식
+			}
+		}
+		
+		System.out.println(D[N][K]);
 	}
 
 }
