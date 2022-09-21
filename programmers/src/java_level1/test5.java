c796935b97bb571476256dc23bd4b6e7eb411915
baehj <101856005+baehj@users.noreply.github.com>
2022-09-21 20:53:44 +0900

    220921 lv.1 약수의 합

---
+++ programmers/src/java_level1/test5.java
@@ -0,0 +1,19 @@
+package java_level1;
+
+public class test5 {
+
+	public static void main(String[] args) {
+		
+		int n = 12;
+		
+		int answer = 0;
+        for(int i=1; i<=n; i++){
+            if (n%i == 0){
+                answer += i;
+            }
+        }
+        
+        System.out.println(answer);
+	
+	}
+}
