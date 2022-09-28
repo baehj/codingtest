fbed0e1545c2fcdf67e56c9b6e062c9e42902b0a
baehj <101856005+baehj@users.noreply.github.com>
2022-09-28 21:37:16 +0900

    220928 lv.1 문자열 내 p와 y의 개수 (수정ok)

--- programmers/src/java_level1/test8.java
+++ programmers/src/java_level1/test8.java
@@ -2,7 +2,8 @@ package java_level1;
 
 public class test8 {
 	// 문자열 내 p와 y의 개수 (정확도테스트 체크)
-
+	
+	/*
 	boolean solution(String s) {
         int pCnt = 0;
         int yCnt = 0;
@@ -25,5 +26,29 @@ public class test8 {
         } 
          
     }
+	*/
+	 	// 샘플케이스를 잘보자! 대소문자 구분할 필요가 있었음!
+	  boolean solution(String s) {
+	        int pCnt = 0;
+	        int yCnt = 0;
+	        
+	        String[] a = s.toLowerCase().split(""); 
+	         
+	        for (int i = 0; i < a.length; i++) {
+	            if(a[i].equals("p")){
+	                pCnt ++;
+	            } else if(a[i].equals("y")){
+	                yCnt++;
+	            }
+	        }
 
+	        if(pCnt == yCnt){
+	            return true;
+	        } else if (pCnt==0 && yCnt==0){
+	            return true;
+	        } else {
+	            return false;
+	        } 
+	         
+	    }
 }
