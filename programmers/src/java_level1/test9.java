0b1d329518ec24882dda32f8b7372d8c2f4e7234
baehj <101856005+baehj@users.noreply.github.com>
2022-10-02 00:02:23 +0900

    221001 lv.1 하샤드 수

--- programmers/src/java_level1/test9.java
+++ programmers/src/java_level1/test9.java
@@ -4,8 +4,22 @@ public class test9 {
 	//하샤드 수
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int x = 1;
+		int originalX = x;
+		boolean answer = false;
+        int sum = 0;
+        while(x != 0){
+            int s = x%10;
+            sum = sum + s;
+            x = x/10;
+        }
+        
+        if (sum != 0 && originalX%sum == 0){
+        	answer = true;
+        }
+        System.out.println(answer);
+        
 	}
 
 }
