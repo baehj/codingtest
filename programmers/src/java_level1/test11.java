d8989a2f05f10db9fdc094cbab876dbf689d3fcb
baehj <101856005+baehj@users.noreply.github.com>
2022-10-03 23:54:09 +0900

    221003 lv.1 문자열을 정수로 바꾸기

---
+++ programmers/src/java_level1/test11.java
@@ -0,0 +1,13 @@
+package java_level1;
+
+public class test11 {
+	// 문자열을 정수로 바꾸기
+	public static void main(String[] args) {
+		
+		 String s = "-1234";
+		 int answer = Integer.parseInt(s);
+		 System.out.println(answer);
+	       
+	}
+
+}
